a60f02536ce96254bfd370d80253f760d3113e79
Lukas Reichmann <lukas@reichmann-info.de>
2019-08-24 11:52:30 +0200

    Added DefaultLogin.getEmail() & .getPassword()

--- app/model/DefaultLogin.ts
+++ app/model/DefaultLogin.ts
@@ -1,3 +1,5 @@
+import DatabaseConnection from '../core/DatabaseConnection';
+import errors from './Errors';
 import Password from './Password';
 import UserManager from './UserManager';
 
@@ -30,9 +32,22 @@ class DefaultLogin {
 
     /**
      * @returns The E-Mail that the login has.
+     *
+     * @throws **DefaultLoginNotFoundError** If the default login does not exist.
      */
-    public getEmail(): string {
-        return '';
+    public async getEmail(): Promise<string> {
+        const result = await DatabaseConnection.query(
+            'SELECT `email` FROM `DefaultLogins` WHERE `DefaultLogins`.`defaultlogin_id` = ?', {
+                parameters: [
+                    this.id
+                ]
+            });
+
+        if (result.length === 1) {
+            return result[0].email;
+        } else {
+            throw this.makeLoginNotFoundError();
+        }
     }
 
     /**
@@ -47,8 +62,24 @@ class DefaultLogin {
         return '';
     }
 
-    public getPassword(): Password {
-        throw new Error();
+    /**
+     * @returns The encrypted password of the user.
+     *
+     * @throws **DefaultLoginNotFoundError** If the default login does not exist.
+     */
+    public async getPassword(): Promise<Password> {
+        const result = await DatabaseConnection.query(
+            'SELECT `password` FROM `DefaultLogins` WHERE `DefaultLogins`.`defaultlogin_id` = ?', {
+                parameters: [
+                    this.id
+                ]
+            });
+
+        if (result.length === 1) {
+            return new Password(result[0].password);
+        } else {
+            throw this.makeLoginNotFoundError();
+        }
     }
 
     /**
@@ -86,6 +117,13 @@ class DefaultLogin {
      */
     public query(queryData: DefaultLogin.DefaultLoginQueryData): DefaultLogin.IDefaultLoginQueryResult {
         return {};
+    }
+
+    /**
+     * @returns A DefaultLoginNotFoundError with the proper user ID.
+     */
+    private makeLoginNotFoundError(): errors.DefaultLoginNotFoundError {
+        return new errors.DefaultLoginNotFoundError(this.id);
     }
 }
 
