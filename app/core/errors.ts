34d215bc950f7a494fb6b7f5cd81efeaaa135a44
Lukas Reichmann <lukas@reichmann-info.de>
2019-08-27 19:54:29 +0200

    Added errors.

---
+++ app/core/errors.ts
@@ -0,0 +1,82 @@
+import Error from './error';
+
+namespace errors {
+
+    /**
+     * An email configuration is in an invalid format.
+     *
+     * The data layout is the following:
+     * ```typescript
+     * {
+     *     name: "<configuration name>"
+     * }
+     * ```
+     */
+    export class InvalidEmailConfigurationError extends Error {
+        public constructor(name: string) {
+            super('INVALID_EMAIL_CONFIGURATION_ERROR',
+            `The E-Mail configuration "${name}" is in an invalid format.`, 500, {
+                name
+            });
+        }
+    }
+
+    /**
+     * A HTML template could not be compiled.
+     *
+     * The data layout is the following:
+     * ```typescript
+     * {
+     *     name: "<template name>"
+     * }
+     * ```
+     */
+    export class TemplateCompileError extends Error {
+        public constructor(name: string) {
+            super('TEMPLATE_COMPILE_ERROR',
+            `The HTML template "${name}" could not be compiled.`, 500, {
+                name
+            });
+        }
+    }
+
+    /**
+     * A HTML could not be generated.
+     *
+     * The data layout is the following:
+     * ```typescript
+     * {
+     *     name: "<template name>"
+     * }
+     * ```
+     */
+    export class HTMLGenerationError extends Error {
+        public constructor(name: string) {
+            super('HTML_GENERATION_ERROR',
+            `The HTML from the template "${name}" could not be generated.`, 500, {
+                name
+            });
+        }
+    }
+
+    /**
+     * Could not send an E-Mail.
+     *
+     * The data layout is the following:
+     * ```typescript
+     * {
+     *     to: "<recipient E-Mail>"
+     * }
+     * ```
+     */
+    export class EmailSendError extends Error {
+        public constructor(to: string) {
+            super('EMAIL_SEND_ERROR',
+            `Could not send an E-Mail to "${to}".`, 500, {
+                to
+            });
+        }
+    }
+}
+
+export = errors;
