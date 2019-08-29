426d6b04f8900fcfad3a8960be2995f06c26d1d4
Lukas Reichmann <lukas@reichmann-info.de>
2019-08-29 14:47:49 +0200

    Added sync variant to load-json.

--- app/core/load-json.ts
+++ app/core/load-json.ts
@@ -1,25 +1,52 @@
 import fs from 'fs';
 import errors from './errors';
 
-/**
- * @param jsonPath The path to the JSON file.
- * @returns The parse JSON object.
- *
- * @throws **JSONParseError** If either the file could not be opened or the contents is invalid JSON.
- */
-async function loadJSON(jsonPath: string): Promise<any> {
-    let raw: string;
+namespace JSONLoader {
+    /**
+     * @param jsonPath The path to the JSON file.
+     * @returns The parse JSON object.
+     *
+     * @throws **JSONParseError** If either the file could not be opened or the contents is invalid JSON.
+     */
+    export async function loadJSON(jsonPath: string): Promise<any> {
+        let raw: string;
 
-    try {
-        raw = await fs.promises.readFile(jsonPath, 'utf-8');
-    } catch (error) {
-        throw new errors.FileNotFoundError(jsonPath);
+        try {
+            raw = await fs.promises.readFile(jsonPath, 'utf-8');
+        } catch (error) {
+            throw new errors.FileNotFoundError(jsonPath);
+        }
+
+        try {
+            return JSON.parse(raw);
+        } catch (error) {
+            throw new errors.JSONParseError();
+        }
     }
 
-    try {
-        return JSON.parse(raw);
-    } catch (error) {
-        throw new errors.JSONParseError();
+    /**
+     * @param jsonPath The path to the JSON file.
+     * @returns The parse JSON object.
+     *
+     * @throws **JSONParseError** If the file contents is invalid JSON.
+     * @throws **FileNotFoundError** If the file could not be found.
+     */
+    export function loadJSONSync(jsonPath: string): any {
+        let raw: string;
+
+        try {
+            raw = fs.readFileSync(jsonPath, 'utf-8');
+        } catch (error) {
+            throw new errors.FileNotFoundError(jsonPath);
+        }
+
+        try {
+            return JSON.parse(raw);
+        } catch (error) {
+            throw new errors.JSONParseError();
+        }
     }
+
 }
-export = loadJSON;
+
+export = JSONLoader;
--- test/integrationtest/core/load-json.test.ts
+++ test/integrationtest/core/load-json.test.ts
@@ -1,26 +1,49 @@
 import path from 'path';
 import errors from '../../../app/core/errors';
-import getRootDirectory from '../../../app/core/get-root-directory';
-import loadJSON from '../../../app/core/load-json';
+import RootDirectory from '../../../app/core/get-root-directory';
+import JSONLoader from '../../../app/core/load-json';
 
 describe('core', () => {
     describe('load-json.ts', () => {
-        it('should correctly parse an existing, valid JSON file', async () => {
-            const p = path.join(await getRootDirectory(), 'test', 'integrationtest', 'res', 'valid-json.json');
-            await expect(loadJSON(p)).resolves.toEqual({
-                test: 'test',
-                number: 1
+        describe('async variant', () => {
+            it('should correctly parse an existing, valid JSON file', async () => {
+                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'valid-json.json');
+                await expect(JSONLoader.loadJSON(p)).resolves.toEqual({
+                    test: 'test',
+                    number: 1
+                });
+            });
+
+            it('should throw while parsing an existing, invalid JSON file', async () => {
+                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'invalid-json.json');
+                await expect(JSONLoader.loadJSON(p)).rejects.toEqual(new errors.JSONParseError());
+            });
+
+            it('should throw while parsing a JSON file that does not exist', async () => {
+                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'not-existing-json.json');
+                await expect(JSONLoader.loadJSON(p)).rejects.toEqual(new errors.FileNotFoundError(p));
             });
         });
 
-        it('should throw while parsing an existing, invalid JSON file', async () => {
-            const p = path.join(await getRootDirectory(), 'test', 'integrationtest', 'res', 'invalid-json.json');
-            await expect(loadJSON(p)).rejects.toEqual(new errors.JSONParseError());
-        });
+        describe('sync variant', () => {
+            it('should correctly parse an existing, valid JSON file', () => {
+                const p = path.join(RootDirectory.getSync(), 'test', 'integrationtest', 'res', 'valid-json.json');
+                expect(JSONLoader.loadJSONSync(p)).toEqual({
+                    test: 'test',
+                    number: 1
+                });
+            });
 
-        it('should throw while parsing a JSON file that does not exist', async () => {
-            const p = path.join(await getRootDirectory(), 'test', 'integrationtest', 'res', 'not-existing-json.json');
-            await expect(loadJSON(p)).rejects.toEqual(new errors.FileNotFoundError(p));
+            it('should throw while parsing an existing, invalid JSON file', () => {
+                const p = path.join(RootDirectory.getSync(), 'test', 'integrationtest', 'res', 'invalid-json.json');
+                expect(() => JSONLoader.loadJSONSync(p)).toThrow(new errors.JSONParseError());
+            });
+
+            it('should throw while parsing a JSON file that does not exist', () => {
+                const p = path.join(RootDirectory.getSync(),
+                    'test', 'integrationtest', 'res', 'not-existing-json.json');
+                expect(() => JSONLoader.loadJSONSync(p)).toThrow(new errors.FileNotFoundError(p));
+            });
         });
     });
 });
