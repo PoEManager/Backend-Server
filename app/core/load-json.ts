f77b672ac6fa7d792bb1c0d74ddbde050559a886
Lukas Reichmann <lukas@reichmann-info.de>
2019-08-30 11:21:21 +0200

    Improved load-json to avoid code duplication.

--- app/core/load-json.ts
+++ app/core/load-json.ts
@@ -9,19 +9,13 @@ namespace JSONLoader {
      * @throws **JSONParseError** If either the file could not be opened or the contents is invalid JSON.
      */
     export async function loadJSON(jsonPath: string): Promise<any> {
-        let raw: string;
-
-        try {
-            raw = await fs.promises.readFile(jsonPath, 'utf-8');
-        } catch (error) {
-            throw new errors.FileNotFoundError(jsonPath);
-        }
-
-        try {
-            return JSON.parse(raw);
-        } catch (error) {
-            throw new errors.JSONParseError();
-        }
+        return new Promise((resolve, reject) => {
+            try {
+                resolve(loadJSONSync(jsonPath));
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
     /**
