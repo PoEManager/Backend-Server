d9598e5feb3f8979851f1a970c64a2341e0783ea
Lukas Reichmann <lukas@reichmann-info.de>
2019-08-30 10:12:07 +0200

    Renamed integrationtest and unittest directories.

--- test/integration/core/config.test.ts
+++ test/integration/core/config.test.ts
@@ -8,53 +8,53 @@ describe('core', () => {
         });
 
         it('should load the configuration from a custom directory', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg1');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg1');
             expect(cfg.test.test).toBe('test');
         });
 
         it('should load the "default" environment if there is no fitting environment', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg2');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg2');
             expect(cfg.test.test).toBe('test');
         });
 
         it('should load no configs if there is not even a default environment fitting environment', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg3');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg3');
             expect(cfg.test).toBeUndefined();
         });
 
         it('should correctly use overrides', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg5');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg5');
             expect(cfg.test.overriddenTest).toBe('overridden');
         });
 
         it('should allow partial configs', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg6');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg6');
             expect(cfg.test.full).toBe('full');
             expect(cfg.test.partial).toBe('partial');
         });
 
         it('should throw if the config is in an invalid schema', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg7'))
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg7'))
                 .toThrow(new errors.ObjectValidationError(['should have required property \'test\'']));
         });
 
         it('should throw if the config metadata is in an invalid schema', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg8'))
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg8'))
                 .toThrow(new errors.ConfigMetaValidationError(['should NOT have additional properties']));
         });
 
         it('should throw if a config file can not be parsed', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg9'))
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg9'))
                 .toThrow(new errors.JSONParseError());
         });
 
         it('should throw if a config environment has a nonexistent dependency', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg10'))
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg10'))
                 .toThrow(new errors.ConfigExtensionError('nonexistent'));
         });
 
         it('should correctly load a config with dependencies', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg11');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg11');
 
             expect(cfg.test.test1).toBe('test1');
             expect(cfg.test.test2).toBe('test2');
@@ -62,7 +62,7 @@ describe('core', () => {
         });
 
         it('should work with a complex example', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg12');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg12');
 
             expect(cfg.test.test1).toBe('test1');
             expect(cfg.test.test2).toBe('test2');
@@ -72,20 +72,20 @@ describe('core', () => {
         });
 
         it('should correctly work if only the extension has sources', () => {
-            const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg13');
+            const cfg = config._loadDbgConfig('test/integration/res/config/cfg13');
             expect(cfg.test.test).toBe('test');
         });
 
         it('should correctly work if there are no configurations (empty array)', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg14')).not.toThrow();
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg14')).not.toThrow();
         });
 
         it('should correctly work if there are no configurations (undefined)', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg15')).not.toThrow();
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg15')).not.toThrow();
         });
 
         it('should correctly work if there are no sources', () => {
-            expect(() => config._loadDbgConfig('test/integrationtest/res/config/cfg16')).not.toThrow();
+            expect(() => config._loadDbgConfig('test/integration/res/config/cfg16')).not.toThrow();
         });
 
         describe('undefined NODE_ENV', () => {
@@ -100,7 +100,7 @@ describe('core', () => {
             });
 
             it('should load the correct configuration when no NODE_ENV is set', () => {
-                const cfg = config._loadDbgConfig('test/integrationtest/res/config/cfg4');
+                const cfg = config._loadDbgConfig('test/integration/res/config/cfg4');
                 expect(cfg.test.test).toBe('test');
             });
         });
--- test/integration/core/load-json.test.ts
+++ test/integration/core/load-json.test.ts
@@ -7,7 +7,7 @@ describe('core', () => {
     describe('load-json.ts', () => {
         describe('async variant', () => {
             it('should correctly parse an existing, valid JSON file', async () => {
-                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'valid-json.json');
+                const p = path.join(await RootDirectory.get(), 'test', 'integration', 'res', 'valid-json.json');
                 await expect(JSONLoader.loadJSON(p)).resolves.toEqual({
                     test: 'test',
                     number: 1
@@ -15,19 +15,19 @@ describe('core', () => {
             });
 
             it('should throw while parsing an existing, invalid JSON file', async () => {
-                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'invalid-json.json');
+                const p = path.join(await RootDirectory.get(), 'test', 'integration', 'res', 'invalid-json.json');
                 await expect(JSONLoader.loadJSON(p)).rejects.toEqual(new errors.JSONParseError());
             });
 
             it('should throw while parsing a JSON file that does not exist', async () => {
-                const p = path.join(await RootDirectory.get(), 'test', 'integrationtest', 'res', 'not-existing-json.json');
+                const p = path.join(await RootDirectory.get(), 'test', 'integration', 'res', 'not-existing-json.json');
                 await expect(JSONLoader.loadJSON(p)).rejects.toEqual(new errors.FileNotFoundError(p));
             });
         });
 
         describe('sync variant', () => {
             it('should correctly parse an existing, valid JSON file', () => {
-                const p = path.join(RootDirectory.getSync(), 'test', 'integrationtest', 'res', 'valid-json.json');
+                const p = path.join(RootDirectory.getSync(), 'test', 'integration', 'res', 'valid-json.json');
                 expect(JSONLoader.loadJSONSync(p)).toEqual({
                     test: 'test',
                     number: 1
@@ -35,13 +35,13 @@ describe('core', () => {
             });
 
             it('should throw while parsing an existing, invalid JSON file', () => {
-                const p = path.join(RootDirectory.getSync(), 'test', 'integrationtest', 'res', 'invalid-json.json');
+                const p = path.join(RootDirectory.getSync(), 'test', 'integration', 'res', 'invalid-json.json');
                 expect(() => JSONLoader.loadJSONSync(p)).toThrow(new errors.JSONParseError());
             });
 
             it('should throw while parsing a JSON file that does not exist', () => {
                 const p = path.join(RootDirectory.getSync(),
-                    'test', 'integrationtest', 'res', 'not-existing-json.json');
+                    'test', 'integration', 'res', 'not-existing-json.json');
                 expect(() => JSONLoader.loadJSONSync(p)).toThrow(new errors.FileNotFoundError(p));
             });
         });
