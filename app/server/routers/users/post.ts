27e89710dd88f3d75b5e70703e24da46d288b589
Lukas Reichmann <lukas@reichmann-info.de>
2019-09-05 17:55:52 +0200

    Removed obsolete error handling.

--- app/server/routers/users/post.ts
+++ app/server/routers/users/post.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import logger from '../../../core/logger';
 import UserManager from '../../../model/user-manager';
 import RouteConfiguration from '../../route-configuration';
-import ServerUtils from '../../server-utils';
 
 const route: RouteConfiguration = {
     method: 'POST',
@@ -19,21 +18,17 @@ const route: RouteConfiguration = {
 };
 
 async function handler(req: express.Request, res: express.Response): Promise<void> {
-    try {
-        const user = await UserManager.create({
-            nickname: req.body.nickname,
-            loginData: {
-                email: req.body.loginData.email,
-                unencryptedPassword: req.body.loginData.password
-            }
-        });
+    const user = await UserManager.create({
+        nickname: req.body.nickname,
+        loginData: {
+            email: req.body.loginData.email,
+            unencryptedPassword: req.body.loginData.password
+        }
+    });
 
-        logger.info(`Created new user: id=${user.getId()};` +
-            `nickname=${req.body.nickname};email=${req.body.loginData.email}`);
-        res.send();
-    } catch (error) {
-        ServerUtils.sendRESTError(res, error);
-    }
+    logger.info(`Created new user: id=${user.getId()};` +
+        `nickname=${req.body.nickname};email=${req.body.loginData.email}`);
+    res.send();
 }
 
 export = route;
