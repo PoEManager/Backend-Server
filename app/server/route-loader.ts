08080aa1ebee4d8869bed651dfac1a8c7dbd4ba0
Lukas Reichmann <lukas@reichmann-info.de>
2019-10-27 13:08:55 +0100

    Added documentation.

--- app/server/route-loader.ts
+++ app/server/route-loader.ts
@@ -6,6 +6,15 @@ import logger from '../core/logger';
 import RootDirectory from '../core/root-directory';
 import RouteConfiguration from './route-configuration';
 
+/* 
+ERROR HANDLING:
+
+The error handling in this file is often done via error codes and not regular error objects. This is because the 
+differentiation between error codes is simpler than the differentiation between regular errors.
+
+The enum 'ErrorCodes' contains all of the error codes.
+*/
+
 namespace RouteLoader {
     const ROUTE_DIR = path.join(__dirname, 'routes');
     const ROUTE_DEF_FILE = 'route-definition.json';
@@ -19,9 +28,24 @@ namespace RouteLoader {
     const ROUTE_DEF_SCHEMA = path.join(SCHEMA_ROOT, 'route-definition.schema.json');
 
     enum ErrorCodes {
-        IS_DIRECTORY,
+        /**
+         * A file is not a regular file.
+         */
+        IS_NOT_FILE,
+
+        /**
+         * `import()` failed.
+         */
         COULD_NOT_IMPORT,
+
+        /**
+         * An object could not be validated.
+         */
         VALIDATION_ERROR,
+
+        /**
+         * A file does not exist.
+         */
         FILE_NOT_FOUND
     }
 
@@ -48,6 +72,11 @@ namespace RouteLoader {
         };
     }
 
+    /**
+     * Similar to fs.readdir(), but returns absolute paths.
+     *
+     * @param dir The directory to read.
+     */
     async function readdir(dir: string): Promise<string[]> {
         const files = fs.readdirSync(dir);
 
@@ -58,6 +87,9 @@ namespace RouteLoader {
         return files;
     }
 
+    /**
+     * Lists all of the route directories (i.e. the child directories of the root route directory).
+     */
     async function listRouteDirs(): Promise<string[]> {
         logger.info(`Loading routes from: ${ROUTE_DIR}.`);
         const files = await readdir(ROUTE_DIR);
@@ -74,13 +106,28 @@ namespace RouteLoader {
         return files;
     }
 
+    /**
+     * Generic function to import a file. Finds the file with the name `file` in the array `routeFiles` and imports it.
+     *
+     * The imported file needs to:
+     * - be in `routeFiles`
+     * - exist
+     * - be a regular file
+     *
+     * Importing is done via the function `import()`.
+     *
+     * For error handling, see the top of the file.
+     *
+     * @param routeFiles All of the files of the route.
+     * @param file The name of the schema file. This file needs to be in the array `routeFiles`.
+     */
     async function importFile(routeFiles: string[], file: string): Promise<any> {
         let index = 0;
 
         for (const routeFile of routeFiles) {
             if (path.basename(routeFile) === file) { // correct file was found
                 if (!fs.statSync(routeFile).isFile()) {
-                    throw makeError(routeFile, ErrorCodes.IS_DIRECTORY);
+                    throw makeError(routeFile, ErrorCodes.IS_NOT_FILE);
                 }
 
                 try {
@@ -98,6 +145,13 @@ namespace RouteLoader {
         throw makeError(file, ErrorCodes.FILE_NOT_FOUND);
     }
 
+    /**
+     * Imports and validates the route definition file (route-definition.json).
+     *
+     * For error handling, see the top of the file.
+     *
+     * @param routeFiles All of the files of the route.
+     */
     async function importRouteDefinition(routeFiles: string[]): Promise<IRouteDefinition> {
         const object = await importFile(routeFiles, ROUTE_DEF_FILE);
         const schema = await import(ROUTE_DEF_SCHEMA);
@@ -113,12 +167,27 @@ namespace RouteLoader {
         return object as IRouteDefinition;
     }
 
+    /**
+     * Imports the route handler file of a route (handler.ts / handler.js).
+     *
+     * @param routeFiles All of the files of the route.
+     */
     async function importHandler(routeFiles: string[]): Promise<{default: RouteHandler}> {
         const object = await importFile(routeFiles, HANDLER_FILE);
 
         return object as {default: RouteHandler};
     }
 
+    /**
+     * Imports and validates a JSON schema.
+     *
+     * This is used to load parameter-, query-, header- and body schemas.
+     *
+     * For error handling, see the top of the file.
+     *
+     * @param routeFiles All of the files of the route.
+     * @param file The name of the schema file. This file needs to be in the array `routeFiles`.
+     */
     async function importSchema(routeFiles: string[], file: string): Promise<any | undefined> {
         try {
             const object = await importFile(routeFiles, file);
@@ -140,6 +209,11 @@ namespace RouteLoader {
         }
     }
 
+    /**
+     * Loads a single route from a single directory.
+     *
+     * @param routeDir The directory of the route.
+     */
     async function loadRoute(routeDir: string): Promise<IRoute> {
         const routeFiles = await readdir(routeDir);
 
@@ -172,6 +246,9 @@ namespace RouteLoader {
         };
     }
 
+    /**
+     * Loads all of the routes.
+     */
     async function loadRoutes(): Promise<IRoute[]> {
         const ret: IRoute[] = [];
 
@@ -186,6 +263,11 @@ namespace RouteLoader {
         return ret;
     }
 
+    /**
+     * Adds all of the routes to the passed router.
+     *
+     * @param router The router that the routes will be added to.
+     */
     export async function addRoutes(router: express.Router) {
         const routes = await loadRoutes();
 
