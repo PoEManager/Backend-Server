2901b54f9823dcb8761ce9f37135b3b5c37f356b
Lukas Reichmann <lukas@reichmann-info.de>
2019-10-28 11:13:29 +0100

    Added 'createdTime' to GET /users.

--- app/server/routes/get-users-id/handler.ts
+++ app/server/routes/get-users-id/handler.ts
@@ -5,22 +5,49 @@ import UserManager from '../../../model/user-manager';
 import errors from '../../errors';
 
 const paramMap: {[key: string]: number} = {
-    nickname: User.QueryData.NICKNAME
+    nickname: User.QueryData.NICKNAME,
+    createdTime: User.QueryData.CREATED_TIME
 };
 
 // Technically not required, if the keys and values are always the same.
 // However, this allows for changes in User.query() without changing the REST interface
 // Also, this decouples the model and the interface.
 const resultMap: {[key: string]: string} = {
-    nickname: 'nickname'
+    nickname: 'nickname',
+    createdTime: 'createdTime'
 };
 
 function urlQueryToUserQueryParams(params: any): User.QueryData[] {
     return _.map(Object.keys(params), key => paramMap[key]);
 }
 
-function resultToBodyObject(result: User.IQueryResult): {[key: string]: string | number | undefined} {
+interface ITemporaryBody {
+    [key: string]: string | Date | number | undefined;
+}
+
+function resultToBodyObject(result: User.IQueryResult): ITemporaryBody {
     return _.mapKeys(result, (value, key) => resultMap[key]);
+}
+
+// transforms the body returned by resultToBodyObject() into one that can be send as result
+function bodyTransformer(inBody: ITemporaryBody): {[key: string]: string | number} {
+    const ret: {[key: string]: string | number} = {};
+
+    for (const key in inBody) {
+        if (key) {
+            const object = inBody[key];
+
+            if (object !== undefined) { // filters out undefined objects
+                if (object instanceof Date) {
+                    ret[key] = object.toISOString();
+                } else { // case for strings + numbers
+                    ret[key] = object;
+                }
+            }
+        }
+    }
+
+    return ret;
 }
 
 async function handler(req: express.Request, res: express.Response): Promise<void> {
@@ -37,7 +64,7 @@ async function handler(req: express.Request, res: express.Response): Promise<voi
     const queryParams = urlQueryToUserQueryParams(req.query);
 
     const result = await user.query(queryParams);
-    const resultBody = resultToBodyObject(result);
+    const resultBody = bodyTransformer(resultToBodyObject(result));
 
     req.locals.logger.info('Query successful.');
     res.send(resultBody);
