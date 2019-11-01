0af04e0fac7c394ca1b1b255b65fca7d4e5daa05
Lukas Reichmann <lukas@reichmann-info.de>
2019-11-01 10:37:10 +0100

    Added SSL/HTTPS capabilities to the server.

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
+import fs from 'fs';
 import http from 'http';
+import https from 'https';
+import path from 'path';
 import config from '../core/config';
 import DatabaseConnection from '../core/database-connection';
 import logger from '../core/logger';
+import RootDirectory from '../core/root-directory';
 import errors from './errors';
 import requestLogger from './middleware/logger';
 import requestId from './middleware/request-id';
@@ -37,7 +41,31 @@ namespace Server {
         });
     }
 
-    function setupRouterAndRoutes() {
+    function createServer(requestListener: http.RequestListener): http.Server | https.Server {
+        if (!config.security.sslSettings.useSSL) {
+            return http.createServer(requestListener);
+        } else {
+            const keyPath = path.resolve(RootDirectory.getSync(), config.security.sslSettings.keyFile);
+            const certPath = path.resolve(RootDirectory.getSync(), config.security.sslSettings.certFile);
+
+            if (!fs.existsSync(keyPath)) {
+                throw new Error(`File ${keyPath} was not found.`);
+            }
+
+            if (!fs.existsSync(certPath)) {
+                throw new Error(`File ${certPath} was not found.`);
+            }
+
+            const options = {
+                key: fs.readFileSync(keyPath),
+                cert: fs.readFileSync(certPath)
+            };
+
+            return https.createServer(options, requestListener);
+        }
+    }
+
+    async function setupRouterAndRoutes() {
         const router = express.Router();
 
         // these two need to be in this order
@@ -45,28 +73,7 @@ namespace Server {
         setupErrorRoute();
         // ======
 
-        RouteLoader.addRoutes(router)
-        .then(() => {
-            server = http.createServer(app!);
-            server.listen(config.basic.port);
-
-            server.on('close', () => {
-                logger.info('Server is closing down.');
-            });
-
-            server.on('error', error => {
-                logger.error(`Error in server: ${error.message}`);
-            });
-
-            server.on('listening', () => {
-                logger.info('Server is now listening.');
-            });
-        })
-        .catch(e => {
-            logger.error('Unexpected error occurred while setting up the routes.');
-            logger.error(e.message);
-            logger.error('The server has not been started.');
-        });
+        await RouteLoader.addRoutes(router);
     }
 
     export function start() {
@@ -80,7 +87,34 @@ namespace Server {
 
         addMiddleWare();
         logMeta();
-        setupRouterAndRoutes();
+        setupRouterAndRoutes()
+        .then(() => {
+            try {
+                server = createServer(app!);
+                server.listen(config.basic.port);
+
+                server.on('close', () => {
+                    logger.info('Server is closing down.');
+                });
+
+                server.on('error', error => {
+                    logger.error(`Error in server: ${error.message}`);
+                });
+
+                server.on('listening', () => {
+                    logger.info('Server is now listening.');
+                });
+            } catch (error) {
+                logger.error('Unexpected error occurred while setting up the server.');
+                logger.error(error.message);
+                logger.error('The server has not been started.');
+            }
+        })
+        .catch(e => {
+            logger.error('Unexpected error occurred while setting up the routes.');
+            logger.error(e.message);
+            logger.error('The server has not been started.');
+        });
     }
 
     export async function stop(): Promise<void> {
